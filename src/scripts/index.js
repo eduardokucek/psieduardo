38bdce7a959992c959b4ebf21d1fdd2fa00d832b
Eduardo <eduardo.kucek@gmail.com>
2024-04-24 16:41:09 -0300

    v0.9 - remove all comments

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,6 @@
 function toggleMode() {
   const html = document.documentElement;
 
-  //   if (html.classList.contains("light")) {
-  //     html.classList.remove("light");
-  //   } else {
-  //     html.classList.add("light");
-  //   }
-
   const img = document.querySelector("#profile__image");
 
   html.classList.toggle("light");
