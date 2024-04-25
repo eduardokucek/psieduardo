613d089ba1c47b719f7452f5d9c4e8fef5184f11
Eduardo <eduardo.kucek@gmail.com>
2024-04-25 10:44:40 -0300

    main/v1.6 - update switch (cursor pointer)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,18 +4,4 @@ function toggleMode() {
   const img = document.querySelector("#profile__image");
 
   html.classList.toggle("light");
-
-  /*if (html.classList.contains("light")) {
-    img.setAttribute("src", "./src/assets/eduardobw.jpg");
-    img.setAttribute(
-      "alt",
-      "Foto preto e branco de Eduardo Kucek sorrindo, usando óculos, de barba, de camiseta branca e paletó azul, e ao fundo uma estante de livros."
-    );
-  } else {
-     img.setAttribute("src", "./src/assets/psieduardocolor.jpg");
-     img.setAttribute(
-       "alt",
-       "Foto em preto e branco de Eduardo Kucek sorrindo, usando óculos, de barba, de camiseta branca e paletó azul, e ao fundo uma estante de livros."
-     );
-   }*/
 }
